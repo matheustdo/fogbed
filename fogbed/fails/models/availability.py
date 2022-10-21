4a3b0c7420073e1b7c242115a5993b6f32aa9504
Matheus Teles <matheustdo1@gmail.com>
2022-10-21 20:20:42 -0300

    feat: extend availability

--- fogbed/fails/models/availability.py
+++ fogbed/fails/models/availability.py
@@ -4,6 +4,7 @@ from fogbed.node.container import Container
 from fogbed.node.instance import VirtualInstance
 import random
 import time, threading
+from fractions import Fraction
 
 class AvailabilityFail(FailModel):
     def __init__(self, availability=0.5, slot_time=2.0):
@@ -21,12 +22,27 @@ class AvailabilityCycler(Intervaler):
         self.thread = threading.Thread(target=self.__set_interval)
         self.running = False
         self.all_containers = self.vi.containers.copy()
-        self.first_cycle = True
+        self.first_slot = True
+        self.slot_number = 0
+        self.cycle_size = 1
+        self.cycle_part = 0
+        self.cycle_step = 0
+        self.average = 0
+
+    def print_average(self, amount: int):
+        self.average += amount
+        print('')
+        print('*** SLOT NUMBER: ' + str(self.slot_number))
+        print('*** AVERAGE: ' + str((self.average / self.slot_number) / len(self.all_containers)) )
+        print('')
+
 
     def action(self):
+        self.slot_number += 1
         all_containers_names = list(self.all_containers.keys())
         all_containers_amount = len(self.all_containers)
-        stop_amount = int(all_containers_amount * self.availability)
+        stop_amount = self.calcule_stop_amount()
+        self.print_average(stop_amount)
         down_sorted_idxs = random.sample(range(0, all_containers_amount), stop_amount)
         down_sorted_names = [all_containers_names[down_sorted_idxs[0]], all_containers_names[down_sorted_idxs[1]]]
         cur_containers_names = list(self.vi.containers.keys())
@@ -34,19 +50,19 @@ class AvailabilityCycler(Intervaler):
 
         for container_name in cur_containers_names:
             ''' Discover which containers will stop '''
-            if (container_name == down_sorted_names[0] or container_name == down_sorted_names[1]):
+            if (container_name in down_sorted_names):
                 to_stop.append(container_name)
         
         for container_name in to_stop:
             ''' Stop containers '''
             self.experiment.remove_docker(container_name)
 
-        if (not self.first_cycle):
+        if (not self.first_slot):
             to_start = []
 
             for container_name in all_containers_names:
                 ''' Discover which containers will start '''
-                if (container_name != down_sorted_names[0] and container_name != down_sorted_names[1] and container_name not in cur_containers_names):
+                if (container_name not in down_sorted_names and container_name not in cur_containers_names):
                     to_start.append(container_name)
                     
             for container_name in to_start:
@@ -56,7 +72,7 @@ class AvailabilityCycler(Intervaler):
                 container = Container(container_name, resources=resources, fail_model=fail_model)
                 self.experiment.add_docker(container, self.vi)
                 
-        self.first_cycle = False
+        self.first_slot = False
 
     def __set_interval(self):
         next_time = time.time() + self.slot_time
@@ -68,8 +84,37 @@ class AvailabilityCycler(Intervaler):
     def is_alive(self):
         return self.running
 
+    def calcule_stop_amount(self):
+        if (self.cycle_step >= self.cycle_size):
+            self.cycle_step = 0
+
+        if (self.cycle_size > 1):
+            if (self.cycle_step < self.cycle_size - self.cycle_part):
+                self.cycle_step += 1
+                return self.cycle_amount
+            elif (self.cycle_step < self.cycle_size):
+                self.cycle_step += 1
+                return self.cycle_amount + 1
+        else:
+            return self.cycle_amount
+
+    def calculate_period_size(self):
+        all_containers_amount = len(self.all_containers)
+        division_factor = round(all_containers_amount * self.availability, 1)
+        integer_part = int(division_factor) 
+        decimal_part = int((division_factor % 1)* 10) 
+
+        if (decimal_part > 0):
+            simplified_parts = Fraction(10, decimal_part)
+            numerator = simplified_parts.numerator
+            denominator = simplified_parts.denominator
+            self.cycle_size = numerator
+            self.cycle_part = denominator
+            self.cycle_amount = integer_part
+
     def start(self):
         print('*** Starting Availability Fail on ' + self.vi.label)
+        self.calculate_period_size()
         self.action()
         self.thread.start()
         self.running = True
