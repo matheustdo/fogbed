87063028a12b56600191ddabcbc262e16e6e8366
Matheus Teles <matheustdo1@gmail.com>
2022-11-11 14:29:53 -0300

    example: add init container timeout

--- collector.py
+++ collector.py
@@ -9,12 +9,12 @@ def current_time():
   return timeit.default_timer() * 1000
 
 
-def sensor_collect(url, min_sample_interval, experiment_duration, is_sensor_active):
+def sensor_collect(url, min_sample_interval, experiment_duration, is_sensor_active, exp_number):
   response = urlopen(url)
   devices_json = json.loads(response.read())
   device_name_list = []
   experiment_start_time = timeit.default_timer() * 1000
-  experiment_start_timestamp = time.time()* 1000
+  experiment_start_timestamp = time.time() * 1000
 
   for device in devices_json:
     device_name_list.append(device["id"])
@@ -27,62 +27,46 @@ def sensor_collect(url, min_sample_interval, experiment_duration, is_sensor_acti
   # Experiment
   while experiment_current_time < experiment_duration and timeit.default_timer() - start_time < 150:
     # Gets all devices data and append to results list
-    time.sleep(sleep_duration)
+    # time.sleep(sleep_duration)
     sample = []
     
     for device_name in device_name_list:
       device_url = url + device_name + "/temperatureSensor"
       response = urlopen(device_url)
-      # actived = is_sensor_active(device_name)
+      actived = is_sensor_active(device_name)
       device_json = json.loads(response.read())
       device = { "id": device_name, 
                   "value": device_json["value"], 
                   "startTime": device_json["startTime"],
                   "expStart": experiment_current_time,
-                  "actived": True }
+                  "actived": actived }
 
       sample.append(device)
 
     results.append(sample)
     experiment_current_time = current_time() - experiment_start_time
-  print(experiment_current_time)
-  print(experiment_duration)
+    
   min = 0
-
-  test = "b"
 
   for result in results:
     for device in result:
       calc = device["expStart"] / 1000 - (device["startTime"] - experiment_start_timestamp) / 1000
       if calc < min:
         min = calc
-
-  for result in results:
-    for device in result:
-      calc = device["expStart"] / 1000 - (device["startTime"] - experiment_start_timestamp) / 1000
-      if calc == min:
-        test = device
         
   min = abs(min)
-  print("min  " + str(min))
-  print("startTime  " + str(test["startTime"]))
-  print("experiment_start_time  " + str(experiment_start_timestamp))
-  print("startTime - experiment_start_time  " + str(test["startTime"] - experiment_start_timestamp))
-  print("startTime / 1000  " + str(test["startTime"] / 1000))
-  print("calc  " + str(test["expStart"] / 1000 - (test["startTime"] - experiment_start_timestamp) / 1000))
-  
-
 
   with open('./examples/experiments/result.csv', 'w') as csvfile:
-    csv.writer(csvfile, delimiter=',').writerow(["id", "expStart", "lastUpdate", "actived"])
+    csv.writer(csvfile, delimiter=',').writerow(["expNumber", "id", "expStart", "lastUpdate", "actived"])
     
     for name in device_name_list:
       for result in results:
         for device in result:
           if name == device["id"]:
-            csv.writer(csvfile, delimiter=',').writerow([device["id"], 
-              device["expStart"] / 1000,
-              (device["expStart"] / 1000 - (device["startTime"] - experiment_start_timestamp) / 1000) + min,
+            csv.writer(csvfile, delimiter=',').writerow([exp_number, 
+              device["id"], 
+              "%.4f" % (device["expStart"] / 1000),
+              "%.4f" % ((device["expStart"] / 1000 - (device["startTime"] - experiment_start_timestamp) / 1000) + min),
               device["actived"]])
             break
           
--- test.py
+++ test.py
@@ -16,15 +16,20 @@ from mininet.log import setLogLevel
 # setLogLevel('info')
 
 url = "http://localhost:8181/cxf/iot-service/devices/"
-min_sample_interval = 200 # publish_time = 2000
-experiment_duration = 30000
+min_sample_interval = 100 
+experiment_duration = 20000
+collect_amout = 1
 
-def wait_url(url, verify):
+def wait_url(url, verify, timeout):
   result = None 
   start_time = timeit.default_timer()
   
-  while (result == None and timeit.default_timer() - start_time < 150):
-    time.sleep(3)
+  while (result == None):
+    if (timeit.default_timer() - start_time > timeout):
+      raise Exception("url timeout")
+
+    time.sleep(2)
+
     try:
       result = urlopen(url)
       if (verify):
@@ -40,7 +45,50 @@ def wait_url(url, verify):
   return timeit.default_timer() - start_time
 
 
-def fot_sensors():
+def init_gateway(gateway):
+  started = False
+  attempt = 0
+  total_time = 0
+  print("# Starting Servicemix")
+
+  while (not started and attempt < 2):
+    attempt += 1
+    
+    try:
+      gateway.cmd("./usr/local/bin/servicemix-init.sh &")
+      gateway.cmd("./opt/servicemix/bin/servicemix &")
+      total_time += round(wait_url(url, False, 80), 1)
+      print(f"## Servicemix started in {total_time}s in {attempt} attempt(s)")
+      started = True
+    except:
+      pass
+
+  if (not started):
+    raise Exception("url timeout")
+
+
+def init_device(device):
+  started = False
+  attempt = 0
+  total_time = 0
+  print(f"# Starting {device.name}")
+
+  while (not started and attempt < 3):
+    attempt += 1
+
+    try:
+      device.cmd(f"java -jar device.jar -di {device.name} &")
+      total_time += round(wait_url(f"{url}{device.name}/temperatureSensor", True, 15), 1)
+      print(f"## Device {device.name} started in {total_time}s in {attempt} attempt(s)")
+      started = True
+    except:
+      pass
+
+  if (not started):
+    raise Exception("url timeout")
+  
+
+def collect_environment(exp_number):
   Services(max_cpu=1, max_mem=4096)
   exp = FogbedExperiment()
 
@@ -49,9 +97,9 @@ def fot_sensors():
   edge = exp.add_virtual_instance('edge', EdgeResourceModel(max_cu=2, max_mu=1024)) #, AvailabilityFail(availability=0.5, slot_time=2, availability_mode=AvailabilityMode.DISCONNECT))
 
   #gateway_cloud = Container('gate_cloud', resources=ResourceModel.XLARGE)
-  gateway_fog = Container('gate_fog', resources=ResourceModel.XLARGE, dimage="gateway:publishing", environment={"COLLECT_TIME": "200","PUBLISH_TIME": "1000"}, port_bindings={1883:1883, 8181:8181, 1099:1099, 8101:8101, 61616:61616, 44444:44444})
-  device_1 = Container('device_1', resources=ResourceModel.SMALL, dimage='device:latest', fail_model=AvailabilityFail(availability=0.5, slot_time=2, availability_mode=AvailabilityMode.DISCONNECT)) 
-  device_2 = Container('device_2', resources=ResourceModel.SMALL, dimage='device:latest') 
+  gateway_fog = Container('gate_fog', resources=ResourceModel.XLARGE, dimage="matheustdo/gateway:publishing", environment={"COLLECT_TIME": "200","PUBLISH_TIME": "1000"}, port_bindings={1883:1883, 8181:8181, 1099:1099, 8101:8101, 61616:61616, 44444:44444})
+  device_1 = Container('device_1', resources=ResourceModel.SMALL, dimage='matheustdo/device:latest', fail_model=AvailabilityFail(availability=0.5, slot_time=2, availability_mode=AvailabilityMode.DISCONNECT)) 
+  device_2 = Container('device_2', resources=ResourceModel.SMALL, dimage='matheustdo/device:latest') 
 
   #exp.add_docker(gateway_cloud, cloud)
   exp.add_docker(gateway_fog, fog)
@@ -67,36 +115,21 @@ def fot_sensors():
 
   try:
       exp.start()
-
-      gateway_fog.cmd("./usr/local/bin/servicemix-init.sh &")
-      gateway_fog.cmd("./opt/servicemix/bin/servicemix &")
-      print("# Starting Servicemix")
-      print(f"## Servicemix started in {round(wait_url(url, False), 1)}s")
-
-      device_1.cmd(f"java -jar device.jar -di {device_1.name} &")
-      print(f"# Starting {device_1.name}")
-      print(f"## Device {device_1.name} started in " + str(round(wait_url(f"{url}{device_1.name}/temperatureSensor", True), 1)) + "s")
-
-      device_2.cmd(f"java -jar device.jar -di {device_2.name} &")
-      print(f"# Starting {device_2.name}")
-      print(f"## Device {device_2.name} started in " + str(round(wait_url(f"{url}{device_2.name}/temperatureSensor", True), 1)) + "s")
+      init_gateway(gateway_fog)
+      init_device(device_1)
+      init_device(device_2)
       
       time.sleep(2)
       fail_controller.start()
 
       def is_sensor_active(device_name):
-        # resp = gateway_fog.cmd(f"timeout 0.1 ping -c {exp.get_docker(device_name).ip}")
-        # index = resp.find("[")
-        # return index  > -1
-        #resp = exp.get_docker(device_name).cmd(f"ifconfig eth0")
-        #index = resp.find("UP")
-        #return index > -1
-        #return True
-        return True
+        resp = gateway_fog.cmd(f"timeout 0.1 ping {exp.get_docker(device_name).ip}")
+        index = resp.find("64 bytes")
+        return index  > -1
 
       print(f"# Starting collect")
-      collect_return = sensor_collect(url, min_sample_interval, experiment_duration, is_sensor_active)
-      # exp.start_cli()
+      collect_return = sensor_collect(url, min_sample_interval, experiment_duration, is_sensor_active, exp_number)
+      print(f"## Collect completed")
   except Exception as e:
       print(e)
   finally:
@@ -105,5 +138,14 @@ def fot_sensors():
 
   return collect_return
 
-print(fot_sensors())
+exp_number = 1
+
+while (exp_number <= collect_amout):
+  result = collect_environment(exp_number)
+
+  while (result == -1):
+    result = collect_environment(exp_number)
+
+  exp_number += 1
+
 # sudo service mosquitto stop ; sudo systemctl stop mosquitto.service
